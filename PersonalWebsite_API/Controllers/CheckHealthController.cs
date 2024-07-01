1849ed452c9c9c62045f680e6c23b42197869e77
Colin Kern <Colin.Kern@trustage.com>
2024-07-01 15:05:08 -0500

    Add project files.

---
+++ PersonalWebsite_API/Controllers/CheckHealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebsite_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckHealthController : ControllerBase
+    {
+    }
+}
